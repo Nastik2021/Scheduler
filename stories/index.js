112ab2781c990dfe05926e9b64e1ce14d15e4d7b
Anastassia <nastiatsv@gmail.com>
2021-12-08 20:08:19 -0800

    added Show component under Appointment

--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,7 @@ import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header.js";
 import Empty from "components/Appointment/Empty.js";
+import Show from "components/Appointment/Show.js";
 
 
 
@@ -162,6 +163,17 @@ storiesOf("InterviewerListItem", module)
     .add("Appointment with Time", () => ( <Appointment time='12pm' />))
     .add("Header", () => ( <Header time="12pm" />))
     .add("Empty", () => (<Empty onAdd={action('onAdd')} /> ))
+    .add("Show", () => (
+      <Show
+        student="Lydia Miller-Jones"
+        interviewer={interviewer}
+        onEdit={action('onEdit')}
+        onDelete={action('onDelete')}
+        />
+        ))
+
+
+    
 
     
 
